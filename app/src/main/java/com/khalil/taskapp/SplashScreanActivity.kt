e2212efe371e8e7ddb270cd6f86853361297d8cf
Khalil-Hamdy <khalilhamdy2001@gmail.com>
2022-07-10 22:02:27 +0200

    sign in & sign up & splash screen

---
+++ app/src/main/java/com/khalil/taskapp/SplashScreanActivity.kt
@@ -0,0 +1,27 @@
+package com.khalil.taskapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.khalil.taskapp.databinding.ActivitySplashScreanBinding
+
+
+class SplashScreanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreanBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this, SignINActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        },2000)
+
+    }
+}
